751eb97389c734b7c7f34e62de3f9e99a197785c
GifftyCode <ulokangozi@yahoo.com>
2024-07-22 15:30:40 +0100

    Methods

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -3,31 +3,32 @@ import { useCart } from '../context/CartContext';
 import { useTitle } from '../hooks/useTitle';
 
 export const Cart = () => {
-  const { total } = useCart();
+  const { total, cartList } = useCart();
+
   useTitle('Cart');
 
-  const products = [
-    {
-      id: 1,
-      name: 'ZenithX Hi-Fi 360 Headphones',
-      price: 149,
-      image: '/assets/images/img1.jpg',
-    },
-    {
-      id: 2,
-      name: 'EchoPods BT200 Bluetooth Earphones',
-      price: 49,
-      image: '/assets/images/img2.jpg',
-    },
-  ];
+  // const products = [
+  //   {
+  //     id: 1,
+  //     name: 'ZenithX Hi-Fi 360 Headphones',
+  //     price: 149,
+  //     image: '/assets/images/img1.jpg',
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'EchoPods BT200 Bluetooth Earphones',
+  //     price: 49,
+  //     image: '/assets/images/img2.jpg',
+  //   },
+  // ];
 
   return (
     <main>
       <section className='cart'>
         <h1>
-          Cart Items: {products.length} / ${total}
+          Cart Items: {cartList.length} / ${total}
         </h1>
-        {products.map((product) => (
+        {cartList.map((product) => (
           <CartCard key={product.id} product={product} />
         ))}
       </section>
