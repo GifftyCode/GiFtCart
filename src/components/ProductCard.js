8840b189ca977ae0419ea55041cf4922e4a9c3a6
GifftyCode <ulokangozi@yahoo.com>
2024-07-22 15:04:57 +0100

    Reducer

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,13 +1,17 @@
+import { useCart } from '../context/CartContext';
 import './ProductCard.css';
+
 export const ProductCard = ({ product }) => {
+  const { addToCart } = useCart();
   const { name, price, image } = product;
+
   return (
     <div className='productCard'>
       <img src={image} alt={name} />
       <p className='name'>{name}</p>
       <div className='action'>
         <p>${price}</p>
-        <button>Add To Cart</button>
+        <button onClick={() => addToCart(product)}>Add To Cart</button>
       </div>
     </div>
   );
